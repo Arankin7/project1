1b5048e5427294f046ce2653aa3578d398c338aa
kgil60 <gilvaryke@gmail.com>
2022-04-18 20:47:29 -0400

    added feature to search for events by city

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const seatGeekAuth = "&client_id=" + seatGeekID + "&client_secret=" + seatGeekSe
 const seatGeekEventUrl = "https://api.seatgeek.com/2/venues?postal_code=";
 const seatGeekPerformerUrl = "https://api.seatgeek.com/2/performers?slug=";
 const seatGeekRecUrl = "https://api.seatgeek.com/2/recommendations/performers?performers.id=";
+const seatGeekEventByCityUrl = "https://api.seatgeek.com/2/events?venue.city="
 
 
 // lyric info
@@ -19,9 +20,14 @@ var recSearchEl = document.querySelector("#recSearchBtn");
 var recListEl = document.querySelector("#recContainer");
 var artistRecEl = document.querySelector("#artistRec");
 
+const eventSearchBtnEl = document.getElementById("eventSearchBtn");
+const eventCityEl = document.getElementById("city");
+const eventContainerEl = document.getElementById("eventContainer")
+
 recSearchEl.addEventListener("click", artistRecSearch)
 lyricSearchEl.addEventListener("click", lyricSearch);
 venueSearchEl.addEventListener("click", venueSearch);
+eventSearchBtnEl.addEventListener("click", eventSearch);
 
 
 function lyricSearch(){
@@ -115,4 +121,49 @@ var displayLyrics = function(lyrics){
     var lyricEl = document.createElement("span");
     lyricEl.innerHTML = lyrics.lyrics;
     lyricContainerEl.appendChild(lyricEl);
+}
+
+function eventSearch() {
+    const city = eventCityEl.value.trim();
+
+    if (!city) {
+        console.log("No venue");
+    } else {
+        eventCityEl.value = "";
+        fetch(seatGeekEventByCityUrl + city + seatGeekAuth).then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    displayEvents(data.events);
+                })
+            }
+        })
+    }
+}
+
+function displayEvents(events) {
+    eventContainerEl.textContent = "";
+    // ul el
+    const eventUlEl = document.createElement("ul")
+    eventUlEl.className = "pure-menu-list";
+
+    for(let i=0; i<events.length; i++) {
+        if(events[i].type == "concert") {
+            // console.log(events[i]);
+
+            // create list elements
+            const eventListItem = document.createElement("li");
+            eventListItem.className = "pure-menu-item";
+
+            const eventLinkEl = document.createElement("a");
+            eventLinkEl.className = "pure-menu-link";
+            eventLinkEl.href = events[i].url;
+            eventLinkEl.textContent = events[i].short_title;
+            eventLinkEl.setAttribute("target", "_blank")
+            eventListItem.appendChild(eventLinkEl);
+
+            eventUlEl.appendChild(eventListItem);
+
+        }
+    }
+    eventContainerEl.appendChild(eventUlEl);
 }
